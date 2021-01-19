aa4e45927c73cec2a9588a82cdfe947c7011217d
Aleksandr Maksimov <sanyamaks14@gmail.com>
2021-01-19 11:05:53 +0400

    Add Sidebar and Content empty components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+import Sidebar from "./components/Sidebar/Sidebar";
+import Content from "./components/Content/Content";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Sidebar />
+      <Content />
+    </div>
+  );
+};
 
 export default App;
