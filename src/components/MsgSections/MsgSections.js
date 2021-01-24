62e078c3357e5eacce401fa59ec0b8ea2b9a6866
Aleksandr Maksimov <sanyamaks14@gmail.com>
2021-01-24 19:00:41 +0400

    Add rendering MsgSections from store data

--- src/components/MsgSections/MsgSections.js
+++ src/components/MsgSections/MsgSections.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./MsgSections.scss";
 import MsgSectionsItem from "./MsgSectionsItem/MsgSectionsItem";
+import { Context } from "../../store/Store";
 
 const MsgSections = () => {
-    return (
-        <ul className="msg-sections">
-            <MsgSectionsItem />
-            <MsgSectionsItem />
-            <MsgSectionsItem />
-            <MsgSectionsItem />
-        </ul>
-    );
+  const [state, dispatch] = useContext(Context);
+  return (
+    <ul className="msg-sections">
+      {state.sections.map((section) => {
+        return (
+          <MsgSectionsItem
+            title={section.title}
+            numberOfMessage={section.messages.length}
+            key={section.id}
+            onClick={() =>
+              dispatch({ type: "SELECT_SECTION", payload: section.id })
+            }
+            isActive={section.id === state.activeSection}
+          />
+        );
+      })}
+    </ul>
+  );
 };
 
 export default MsgSections;
--- src/components/MsgSections/MsgSectionsItem/MsgSectionsItem.js
+++ src/components/MsgSections/MsgSectionsItem/MsgSectionsItem.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./MsgSectionsItem.scss";
 import { ReactComponent as Logo } from "../../../assets/icons/icon-archive.svg";
+import cn from "classnames";
 
-const MsgSectionsItem = () => {
+const MsgSectionsItem = (props) => {
+  const { title, numberOfMessage, onClick, isActive } = props;
   return (
-    <li className="msg-sections-item">
-      <button className="msg-sections-item__button">
+    <li
+      className={cn("msg-sections-item", {
+        "msg-sections-item_active": isActive,
+      })}
+    >
+      <button className="msg-sections-item__button" onClick={onClick}>
         <Logo className="msg-sections-item__icon" />
-        <p className="msg-sections-item__title">Title (100)</p>
+        <p className="msg-sections-item__title">
+          {title} ({numberOfMessage})
+        </p>
       </button>
     </li>
   );
